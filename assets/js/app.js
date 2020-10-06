c0e6bdf00c48990678a0f68836a781bff46b0f56
Aung Myat Oo <aungmyatoo2580@gmail.com>
2020-10-07 01:11:20 +0630

    Final Project

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,13 +94,54 @@ $('.testimonial-box').slick({
   });
 
 let screenHeight = $(window).height();
-console.log(screenHeight);
 $(window).scroll(function() {
     let currentPosition = $(this).scrollTop();
-    console.log(currentPosition);
     if(currentPosition > screenHeight - 200) {
         $('nav').addClass('fixed-nav');
     } else {
         $('nav').removeClass('fixed-nav');
     }
 });
+$('.single-item').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$(window).on("load",function() {
+  $('.loading').fadeOut(500,function() {
+    $(this).remove();
+  })
+})
