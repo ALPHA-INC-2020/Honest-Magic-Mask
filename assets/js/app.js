72af45afe83a2dbdb6f2606f7ea8ac769ee22492
Aung Myat Oo <aungmyatoo2580@gmail.com>
2020-10-08 16:31:23 +0630

    fixed error of Nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,82 +1,37 @@
-let testimonial = document.querySelector('.testimonial-box');
 
-let reviews = [
-    {
-        description: "Black Gold Mask လေးက ၀က်ခြံလေးတွေကို တော်တော်ကောင်းလာတယ်၊၊ ပြီးတော့ အသားအရည်လေးလည်းကြည်လာပါတယ်။",
-        name: "Min Thu Kyaw"
-    },
-    {
-        description: "အရမ်းကြိုက်ပါတယ်။ ညီမက dry skinမို့ ငှက်သိုက်လေးကို ပိုပြီးကြိုက်ပါတယ်။",
-        name: "Sandar"
-    },
-    {
-        description: "ညီမ၀က်ခြံလေးက အတော်လေးကို သက်သာလာပါတယ်။",
-        name: "Sandar"
-    },
-    {
-        description: "Sis‌ရေ maskလေးက သုံးရတာအဆင်ပြေတယ်နော်။ အမနဲ့တည့်လို့လားမသိဘူး အခု(၃)ခုပဲ သုံးရသေးတယ်၊၊ ၀င်းပြီး တကယ်glowလာတယ်နော်။",
-        name: "Sandar"
+  
+  let screenHeight = $(window).height();
+  $(window).scroll(function() {
+    let currentPosition = $(this).scrollTop();
+    if(currentPosition > screenHeight - 200) {
+      $('nav').addClass('fixed-nav');
+    } else {
+      $('nav').removeClass('fixed-nav');
     }
-];
-
-reviews.forEach(review => {
-    testimonial.innerHTML += `
-        <div class="col-md-12 mt-3 position-relative wow animate__zoomInDown">
-            <span>
-                <img src="assets/img/pic15.png" width="25px" alt="">
-            </span>
-            <p class="ml-4 mb-0">${review.description}<img src="assets/img/pic16.png" class='quote' width="25px" alt=""></p>
-            <div class="col-12 mx-auto">
-                <hr class="my-4" style="border-width: 2px !important;">
-            </div>
-            <div class="d-flex align-items-center">
-                <img src="./assets/img/pic14.png" class="ml-2" width="60px" alt="">
-                <p class="font-weight-bold mb-0 ml-3">${review.name}</p>
-            </div>
-        </div>
-    `
-});
-
-var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animate__animated', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null // optional scroll container selector, otherwise use window
-  }
-);
-wow.init();
-
-$('.testimonial-box').slick({
+  });
+  $('.single-item').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true,
-          arrows: false,
+          dots: false,
+          arrows: true,
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
         }
       },
       {
@@ -84,7 +39,8 @@ $('.testimonial-box').slick({
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: false,
+          dots: false,
+          arrows: true
         }
       }
       // You can unslick at a given breakpoint now by adding:
@@ -92,56 +48,11 @@ $('.testimonial-box').slick({
       // instead of a settings object
     ]
   });
-
-let screenHeight = $(window).height();
-$(window).scroll(function() {
-    let currentPosition = $(this).scrollTop();
-    if(currentPosition > screenHeight - 200) {
-        $('nav').addClass('fixed-nav');
-    } else {
-        $('nav').removeClass('fixed-nav');
-    }
-});
-$('.single-item').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-$(window).on("load",function() {
-  $('.loading').fadeOut(500,function() {
-    $(this).remove();
-  })
-})
+  $(window).on("load",function() {
+    $('.loading').fadeOut(500,function() {
+      $(this).remove();
+    })
+  });
+  AOS.init();
+  
+  
